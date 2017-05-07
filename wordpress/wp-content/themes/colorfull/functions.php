2c5df42930b6f8c210a2aca7d5deed3294f248f0
Céline Thiry <thiry.celi@gmail.com>
2017-05-07 20:24:41 +0200

    add a comment : use the same syntax to a js file in the head

--- wordpress/wp-content/themes/colorfull/functions.php
+++ wordpress/wp-content/themes/colorfull/functions.php
@@ -1,10 +1,11 @@
 <?php
 
-// Function which add meta, script, styles automatically in the head
+// Function which add meta, scripts, styles automatically in the head
 function colorfull_script_enqueue() {
 
 	// Theme stylesheet.
 	wp_enqueue_style('colorfull-style', get_theme_file_uri( '/css/colorfull.css' ), array(), '1.0.0', 'all');
+	// Theme scritp >> use a similar syntax as in the stylesheet just on top.
 
 }
 
